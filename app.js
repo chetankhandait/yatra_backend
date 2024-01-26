2d193916c1b4e128cd9289fe58c07d179c208337
chetankhandait <chetankhandait16@gmail.com>
2024-01-26 12:30:22 +0530

    feat :sever changes

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.post("/api/create-checkout-session", async (req, res) => {
         allowed_countries: ["US", "CA", "GB", "AU", "IN"], // Specify allowed non-Indian countries for shipping
       },
       billing_address_collection: "required", // Collect billing address information
-      success_url: "http://localhost:5173",
+      success_url: "https://yatrav-1.vercel.app/",
       cancel_url: " https://yatrav-1.vercel.app/cancel"
     });
 
